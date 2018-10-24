9038a665449be1af6bc0f18170d65948f2eb6793
emcd123 <mcdonnell.owen1@gmail.com>
2018-10-24 22:41:09 +0100

    Refactoring Selection Sort

--- cpp/sorting/selectionSort.cpp
+++ cpp/sorting/selectionSort.cpp
@@ -3,16 +3,11 @@
 #include <iostream>
 #include <stdlib.h> //To use random number functions
 
-class SelectionSort{
- public:
-    void Swap(int &p1, int &p2 );
-    void Sort( int* array, int arrSize);
-    int findSmallest(int* array,int arrSize, int start);
-    SelectionSort(void);
-    ~SelectionSort(void);
-};
+#include "selectionSort.h"
+
 SelectionSort::SelectionSort(void){
 }
+
 SelectionSort::~SelectionSort(){
 }
 
@@ -24,6 +19,7 @@ void SelectionSort::Sort(int* array, int arrSize){
         Swap(array[i], array[key]);
     }
 }
+
 int SelectionSort::findSmallest(int* array,int arrSize, int start){
   int smallest = start;
   for(int i=start; i<arrSize; i++){
@@ -33,29 +29,9 @@ int SelectionSort::findSmallest(int* array,int arrSize, int start){
     }
     return smallest;
 }
+
 void SelectionSort::Swap(int &p1, int &p2) {
      int temp = p1;
      p1 = p2;
      p2 = temp;
-}
-
-int main(){
-  const int n = 5;
-  int array[n];
-  /* initialize random seed: */
-  srand (time(NULL));
-  //populate array with random integers
-  for(int i = 0; i< n; i++){
-    array[i] = rand() %10;
-   }
-  std::cout << "Unsorted random generated list:";
-  for(int j= 0; j< n; j++){
-    std::cout << array[j];
-   }
-   SelectionSort sSort;
-   sSort.Sort(array, n);
-     std::cout << "\n Sorted random generated list: ";
-  for(int j= 0; j< n; j++){
-    std::cout << array[j];
-  }
 }
---
+++ cpp/sorting/selectionSort.h
@@ -0,0 +1,8 @@
+class SelectionSort{
+ public:
+    void Swap(int &p1, int &p2 );
+    void Sort( int* array, int arrSize);
+    int findSmallest(int* array,int arrSize, int start);
+    SelectionSort(void);
+    ~SelectionSort(void);
+};
